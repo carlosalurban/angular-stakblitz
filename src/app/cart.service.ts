b98a1f9d1a08ad14c305238624d11c6265bd7636
carlosalurban <carlosalvarezurbano@gmail.com>
2020-01-21 10:54:23 +0100

    4 files modified, 3 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,4 +1,5 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   provideIn: 'root'
@@ -6,7 +7,9 @@ import { Injectable } from '@angular/core';
 export class CartService {
   items = [];
 
-  constructor() { }
+  constructor(
+    private http: HttpClient
+  ) { }
   addToCart(product) {
     this.items.push(product);
   }
@@ -16,5 +19,8 @@ export class CartService {
   clearCart() {
     this.items = [];
     return this.items;
+  }
+  getShippingPrices(){
+    return this.http.get('/assets/shipping.json');
   }
 }
